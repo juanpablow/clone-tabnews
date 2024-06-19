d7774e573e0bcee95b3d4b3ec1c444ad18881a18
juanpablow <juan.santana.macedo10@outlook.com>
2024-06-19 16:49:50 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 2 + 2 as sum;");
+  console.log(result);
   res.status(200).json({ key: "value" });
 }
 
