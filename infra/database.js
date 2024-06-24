de2f0eb8c4b07d61a0f1f9106f2c4aac8de2a08f
juanpablow <juan.santana.macedo10@outlook.com>
2024-06-23 21:03:10 -0300

    makes `database.js` `client.connect()` more robust to handle errors

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result.rows[0];
   } catch (e) {
-    console.error("Error executing query", e);
+    console.error(e);
+    throw e;
   } finally {
     await client.end();
   }
